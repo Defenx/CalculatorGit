f02df554d4de4eacbf2c1fd2fd8d89740bf64b59
Konstantin Nosyrev <nosyrev_k@sofiadoors.com>
2024-03-28 21:58:34 +0300

    added try-catch

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,4 +2,12 @@ public class Calculator {
     int add(int a, int b) {
         return a + b;
     }
+
+    double divide (int a, int b) {
+        try {
+            return a / b;
+        } catch (Exceprion e) {
+            System.out.println(e);
+        }
+    }
 }
